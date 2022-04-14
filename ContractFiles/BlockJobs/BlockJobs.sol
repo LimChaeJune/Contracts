80e9e06098a18146f30a476d3d2b5f6861e6e495
LimChaeJune <30399915+LimChaeJune@users.noreply.github.com>
2022-04-14 17:57:02 +0900

    Create BlockJobs.sol

---
+++ ContractFiles/BlockJobs/BlockJobs.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BJC is ERC20 {
+    //배포 전 openzepplin decimals setting 필수;
+    constructor(string memory _name, string memory _symbol, uint _initSupply) ERC20(_name, _symbol){
+        _mint(msg.sender, _initSupply);
+    }
+
+    function approveByGSN(address _owner, address spender, uint256 amount) external returns (bool) {
+        _approve(_owner, spender, amount);
+        return true;
+    }
+
+
+}
