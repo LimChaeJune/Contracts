40c444a7934e813d3ab527755b0625d0b54eca43
LimChaeJune <30399915+LimChaeJune@users.noreply.github.com>
2022-04-22 17:47:51 +0900

    토큰 생성, 계약 컨트랙트

--- ContractFiles/BlockJobs/BlockJobsCoin.sol
+++ ContractFiles/BlockJobs/BlockJobsCoin.sol
@@ -8,12 +8,10 @@ contract BlockJobsCoin is ERC20 {
     //배포 전 openzepplin decimals setting 필수;
     constructor(string memory _name, string memory _symbol, uint _initSupply) ERC20(_name, _symbol){
         _mint(msg.sender, _initSupply);
-    }
+    }  
 
-    function approveByGSN(address _owner, address spender, uint256 amount) external returns (bool) {
+    function CoinApprove(address _owner, address spender, uint256 amount) external returns (bool) {
         _approve(_owner, spender, amount);
         return true;
     }
-
-
 }
